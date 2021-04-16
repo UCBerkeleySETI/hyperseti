abc9f9557ce5656a55e88369bd62fc4678689795
Richard Elkins <richard.elkins@gmail.com>
2021-04-16 12:04:26 -0500

    Update test_data.py

--- test/test_data.py
+++ test/test_data.py
@@ -1,7 +1,7 @@
 from hyperseti.data import from_fil, from_h5
-from file_defs import *
+from file_defs import voyager_fil, voyager_h5
 
-d = from_fil(VOYAFIL)
+d = from_fil(voyager_fil)
 
 print(d)
 print(d.attrs)
@@ -9,7 +9,7 @@ print(d.dims)
 print(d.scales)
 print(d.data.shape)
 
-d = from_h5(VOYAH5)
+d = from_h5(voyager_h5)
 
 print(d)
 print(d.attrs)
