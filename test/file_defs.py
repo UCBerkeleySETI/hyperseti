b95c19b692debb5a103a47be25eed71955007c89
Richard Elkins <richard.elkins@gmail.com>
2021-04-16 11:11:43 -0500

    Update file_defs.py

--- test/file_defs.py
+++ test/file_defs.py
@@ -1,7 +1,6 @@
 import os
 
 TESTDIR = os.path.split(os.path.abspath(__file__))[0]
-VOYAH5 = 'Voyager1.single_coarse.fine_res.h5'
-VOYAH5FLIPPED = 'Voyager1.single_coarse.fine_res.flipped.h5'
-VOYAFIL = 'Voyager1.single_coarse.fine_res.fil'
-
+VOYAH5 = TESTDIR + '/Voyager1.single_coarse.fine_res.h5'
+VOYAH5FLIPPED = TESTDIR + '/Voyager1.single_coarse.fine_res.flipped.h5'
+VOYAFIL = TESTDIR + '/Voyager1.single_coarse.fine_res.fil'
