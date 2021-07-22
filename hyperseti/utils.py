10d4906c9ba759000cfb8509f609c7014f34f0c8
Danny Price <dan@thetelegraphic.com>
2021-07-22 17:25:18 +1000

    Unit tests for datwrapper update

--- hyperseti/utils.py
+++ hyperseti/utils.py
@@ -3,6 +3,11 @@ import numpy as np
 from functools import wraps
 from inspect import signature
 from astropy.units import Unit
+import numpy as np
+import cupy as cp
+import pandas as pd
+import warnings
+import copy
 
 from .data_array import DataArray
 from .dimension_scale import DimensionScale, TimeScale
@@ -92,9 +97,11 @@ def datwrapper(dims=None, *args, **kwargs):
     
     wrapped funcion must have data as first output, and metadata dict
     
+    if wrapped function supplies 'output_dims' then these will be used.
+    
     Notes:
-        Specific for hyperseti, this will also generate df and dt from
-        dimension scales. 
+        Specific for hyperseti, this will also generate frequency_step 
+        and time_step from dimension scales.
     """
     def _datwrapper(func, *args, **kwargs):
         func_name = func.__name__
@@ -108,7 +115,8 @@ def datwrapper(dims=None, *args, **kwargs):
                 # Copy attribute key:values over 
                 for k, v in d.attrs.items():
                     metadata[k] = v
-                metadata['dims'] = dims
+                metadata['input_dims']  = d.dims # NB: also only intended to be used by the wrapper
+                                                 # But we do supply this to the function just in case
                 for dim in d.dims:
                     if dim == 'time':
                         metadata['time_start']  = d.time.time_start
@@ -133,36 +141,79 @@ def datwrapper(dims=None, *args, **kwargs):
                     # Try and get metadata by looking up argument and finding index
                     func_params = list(signature(func).parameters.keys())
                     md_idx = func_params.index('metadata')
-                        
+                    metadata = func_params[md_idx]
+                    #print(func_params)
+                    #raise RuntimeError(f"<{func_name}> wrapped function does not have a 'metadata' parameter")
+                    
             # OUTPUT MODIFYING
             output = func(*args, **kwargs)
-            if dims is not None:
-                logger.debug(f"<{func_name}> Generating DataArray from function output, {dims}")
-                new_output = []
-                new_data = output[0]
-                new_md   = output[1]
-                logger.debug(f"<{func_name}> data shape: {new_data.shape}")
+            # First, check if the function returns nothing, a bare numpy/cupy array, or pandas array
+            if output is None:
+                return output
+            elif isinstance(output, (np.ndarray, cp.core.core.ndarray)):
+                if dims is not None:
+                    warnings.warn(f"<{func_name}> dimensions supplied, but function returns bare numpy array (no metadata).", RuntimeWarning)
+                return output
+            elif isinstance(output, pd.DataFrame):
+                if dims is not None:
+                    warnings.warn(f"<{func_name}> dimensions supplied, but function returns pandas Dataframe (no metadata).", RuntimeWarning)
+                return output                
+            elif isinstance(output, (list, tuple)):
+                # Check to see if we can apply original dimensions
+                # This requires input to be a DataArray, and output to have array + metadata
+                if dims is None:
+                    if len(output >= 2):
+                        has_array = isinstance(output[0], (np.ndarray, cp.core.core.ndarray))
+                        has_metadata = isinstance(output[1], dict)
+                        if has_array and has_metadata and isinstance(args[0], DataArray):
+                            # In case the wrapped function manually added output_dims
+                            if 'output_dims' in output[1].keys():
+                                _dims = output[1]['output_dims']
+                            # Otherwise, let's use the dimensions of the input DataArray
+                            else:
+                                _dims = args[0].dims
+                else:
+                    _dims = dims          
+                # Now, if dims were found, let's use those to generate a DataArray
+                if _dims is not None:
+                    logger.debug(f"<{func_name}> Generating DataArray from function output, {_dims}")
+                    new_output = []
+                    new_data = output[0]
+                    new_md   = output[1]
+                    new_md['output_dims'] = _dims  # Add output_dims to metadata out
+                    
+                    # Create DataArray attribute dict, which will not include start/stop scales or dims
+                    array_md = copy.deepcopy(new_md)
+                    array_md.pop('input_dims', 0)
+                    array_md.pop('output_dims', 0)
+                    logger.debug(f"<{func_name}> data shape: {new_data.shape}")
 
-                scales = {}
-                for dim_idx, dim in enumerate(dims):
-                    nstep = new_data.shape[dim_idx]
-                    if dim == 'time':
-                        time_start, time_step = new_md["time_start"], new_md["time_step"]
-                        scales[dim] = TimeScale('time', time_start.value, time_step.to('s').value, 
-                                           nstep, time_format=time_start.format, time_delta_format='sec')
-                    else:
-                        scale_start, scale_step = new_md.get(f"{dim}_start", 0), new_md.get(f"{dim}_step", 0)
-                        logger.debug(f"{dim} {scale_start}")
-                        scale_unit = None if np.isscalar(scale_start) else scale_start.unit
-                        scales[dim] = DimensionScale(dim, scale_start, scale_step, 
-                                               nstep, units=scale_unit)
-                darr = DataArray(new_data, dims, scales=scales, attrs=new_md)
-                new_output.append(darr)
-                new_output.append(new_md)
-                for op in output[2:]:
-                    new_output.append(op)
-                return new_output 
+                    scales = {}
+                    for dim_idx, dim in enumerate(_dims):
+                        nstep = new_data.shape[dim_idx]
+                        if dim == 'time':
+                            time_start, time_step = array_md.pop("time_start"), array_md.pop("time_step")
+                            scales[dim] = TimeScale('time', time_start.value, time_step.to('s').value, 
+                                               nstep, time_format=time_start.format, time_delta_format='sec')
+                        else:
+                            scale_start, scale_step = array_md.pop(f"{dim}_start", 0), array_md.pop(f"{dim}_step", 0)
+                            logger.debug(f"{dim} {scale_start}")
+                            scale_unit = None if np.isscalar(scale_start) else scale_start.unit
+                            scales[dim] = DimensionScale(dim, scale_start, scale_step, 
+                                                   nstep, units=scale_unit)
+                    darr = DataArray(new_data, _dims, scales=scales, attrs=array_md)
+                    new_output.append(darr)
+                    new_output.append(new_md)
+                    for op in output[2:]:
+                        new_output.append(op)
+                    return new_output 
+                # Otherwise, we return the un-edited output
+                else:
+                    return output
             else:
-                return output      
+                if dims is not None:
+                    t = type(output)
+                    warnings.warn(f"<{func_name}> dimensions supplied, but function returns {t} which can't be a DataArray.", RuntimeWarning)
+                return output   
         return inner
     return _datwrapper
