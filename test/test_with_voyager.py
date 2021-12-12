de43fd9c2deeabac7bf12b5728be3bb49719dde8
Richard Elkins <richard.elkins@gmail.com>
2021-12-12 13:27:21 -0600

    Add module-level comments
    Try to get results that are similar to those of turbo_seti with our standard Voyager 1 .h5 file.

--- test/test_with_voyager.py
+++ test/test_with_voyager.py
@@ -1,3 +1,21 @@
+"""
+Try to get results that are similar to those of turbo_seti with our standard Voyager 1 .h5 file.
+
+# ----------------------------------------------------
+# File ID: xx.h5 
+# ----------------------------------------------------
+# Source:Voyager1
+# MJD: 57650.782094907408       RA: 17h10m03.984s       DEC: 12d10m58.8s
+# DELTAT:  18.253611    DELTAF(Hz):  -2.793968  max_drift_rate:   4.000000      obs_length: 292.057776
+# --------------------------
+# Top_Hit_#     Drift_Rate      SNR     Uncorrected_Frequency   Corrected_Frequency     Index   freq_start      freq_end        SEFD    SEFD_freq       Coarse_Channel_Number   Full_number_of_hits 
+# --------------------------
+001      -0.392226      156.379913         8419.319368     8419.319368  64651      8419.321003     8419.317740  0.0           0.000000  0       77634
+002      -0.373093      1258.281250        8419.297028     8419.297028  72647      8419.298662     8419.295399  0.0           0.000000  0       77634
+003      -0.392226      159.497269         8419.274374     8419.274374  80755      8419.276009     8419.272745  0.0           0.000000  0       77634
+
+"""
+
 import os
 import logbook
 from hyperseti import find_et
