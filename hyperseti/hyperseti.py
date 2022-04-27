5efd3a76483810d097b8ad49db62d8ba529da823
Danny Price <dan@thetelegraphic.com>
2022-04-27 18:17:21 +0800

    Updates to hyperseti.py API
    run_pipeline now takes config dictionary, and passes via kwargs to called functions

--- hyperseti/hyperseti.py
+++ hyperseti/hyperseti.py
@@ -8,6 +8,7 @@ import h5py
 from astropy import units as u
 import setigen as stg
 from blimpy.io import sigproc
+from copy import deepcopy
 
 from .dedoppler import dedoppler
 from .normalize import normalize
@@ -26,69 +27,77 @@ os.environ['NUMEXPR_MAX_THREADS'] = '8'
 
 @datwrapper(dims=(None))
 @on_gpu
-def run_pipeline(data, metadata, max_dd=4.0, min_dd=None, threshold=30.0, min_fdistance=None, 
-                 n_boxcar=1, merge_boxcar_trials=True, apply_normalization=True,
-                 kernel='dedoppler', gpu_id=0):
+def run_pipeline(data, metadata, config, gpu_id=0):
     """ Run dedoppler + hitsearch pipeline 
     
     Args:
         data (np.array): Numpy array with shape (N_timestep, N_channel)
         metadata (dict): Metadata dictionary, should contain 'df' and 'dt'
                          (frequency and time resolution), as astropy quantities
-        max_dd (float): Maximum doppler drift in Hz/s to search out to.
-        min_dd (float): Minimum doppler drift to search.
-        n_boxcar (int): Number of boxcar trials to do, width 2^N e.g. trials=(1,2,4,8,16)
-        merge_boxcar_trials (bool): Merge hits of boxcar trials to remove 'duplicates'. Default True.
-        apply_normalization (bool): Normalize input data. Default True. Required True for S/N calcs.
-        threshold (float): Threshold value (absolute) above which a peak can be considered
-        min_fdistance (int): Minimum distance in pixels to nearest peak along frequency axis
-        min_ddistance (int): Minimum distance in pixels to nearest peak along doppler axis
-        kernel (str): which GPU kernel to employ for searching.
         gpu_id (int): GPU device ID to use.
     
     Returns:
         (dedopp, metadata, peaks): Array of data post dedoppler (at final boxcar width), plus
                                    metadata (dict) and then table of hits (pd.Dataframe).
     """
-    
+    config = deepcopy(config)
     t0 = time.time()
     attach_gpu_device(gpu_id)
     logger.debug(data.shape)
     N_timesteps = data.shape[0]
-    _threshold = threshold * np.sqrt(N_timesteps)
+
+    # Check if we have a slice 
     
+    # Calculate order for argrelmax (minimum distance from peak)
+    min_fdistance = config['hitsearch'].get('min_fdistance', None)
+    max_dd = config['dedoppler']['max_dd']
     if min_fdistance is None:
-        print(metadata)
         deltaf = metadata['frequency_step'].to('Hz').value
         deltat = metadata['time_step'].to('s').value
         n_int  = data.shape[0]
 
         min_fdistance = int(np.abs(deltat * n_int * max_dd / deltaf))
+        config['hitsearch']['min_fdistance'] = min_fdistance
         logger.info(f"<run_pipeline>: min_fdistance calculated to be {min_fdistance} bins")
 
     # Apply preprocessing normalization
-    if apply_normalization:
-        mask = sk_flag(data, metadata, return_space='gpu')
+    if config['preprocess'].get('normalize', False):
+        if config['preprocess'].get('sk_flag', False):
+            mask = sk_flag(data, metadata, return_space='gpu')
+        else:
+            mask = None
         data = normalize(data, mask=mask, return_space='gpu')
     
     peaks = create_empty_hits_table()
     
+    n_boxcar = config['pipeline'].get('n_boxcar', 1)
     boxcar_trials = map(int, 2**np.arange(0, n_boxcar))
+    
+    _threshold0 = deepcopy(config['hitsearch']['threshold'])
     for boxcar_size in boxcar_trials:
         logger.info(f"--- Boxcar size: {boxcar_size} ---")
-        dedopp, md = dedoppler(data, metadata, boxcar_size=boxcar_size,  boxcar_mode='sum', kernel=kernel,
-                                     max_dd=max_dd, min_dd=min_dd, return_space='gpu')
+        config['dedoppler']['boxcar_size'] = boxcar_size
+        
+        # Check if kernel is computing DD + SK
+        kernel = config['dedoppler'].get('kernel', None)
+        if kernel == 'ddsk':
+            dedopp, dedopp_sk, _md = dedoppler(data, metadata, return_space='gpu', **config['dedoppler'])
+            config['hitsearch']['sk_data'] = dedopp_sk   # Pass to hitsearch
+        else:
+            dedopp, _md = dedoppler(data, metadata, return_space='gpu', **config['dedoppler'])
+            dedopp_sk = None
         
         # Adjust SNR threshold to take into account boxcar size and dedoppler sum
         # Noise increases by sqrt(N_timesteps * boxcar_size)
-        _threshold = threshold * np.sqrt(N_timesteps * boxcar_size)
-        _peaks = hitsearch(dedopp, threshold=_threshold, min_fdistance=min_fdistance)
+        config['hitsearch']['threshold'] = _threshold0 * np.sqrt(N_timesteps * boxcar_size)
+        _peaks = hitsearch(dedopp, **config['hitsearch'])
+        logger.debug(f"{peaks}")
         
         if _peaks is not None:
             _peaks['snr'] /= np.sqrt(N_timesteps * boxcar_size)
             peaks = pd.concat((peaks, _peaks), ignore_index=True)
             
-    if merge_boxcar_trials:
+    if config['pipeline']['merge_boxcar_trials']:
         peaks = merge_hits(peaks)
     t1 = time.time()
     
@@ -96,9 +105,7 @@ def run_pipeline(data, metadata, max_dd=4.0, min_dd=None, threshold=30.0, min_fd
     return peaks
             
 
-def find_et(filename, filename_out='hits.csv', gulp_size=2**19, max_dd=4.0, min_dd=0.001, 
-            min_fdistance=None,  threshold=20.0,
-            n_boxcar=1, kernel='dedoppler', gpu_id=0, *args, **kwargs):
+def find_et(filename, pipeline_config, gulp_size=2**20, filename_out='hits.csv', gpu_id=0, *args, **kwargs):
     """ Find ET, serial version
     
     Wrapper for reading from a file and running run_pipeline() on all subbands within the file.
@@ -121,12 +128,14 @@ def find_et(filename, filename_out='hits.csv', gulp_size=2**19, max_dd=4.0, min_
         Passes keyword arguments on to run_pipeline(). Same as find_et but doesn't use dask parallelization.
     """
     t0 = time.time()
-    logger.debug("find_et: At entry, filename_out={}, gulp_size={}, max_dd={}, min_dd={}, threshold={}, n_boxcar={}, kernel={}, gpu_id={}"
-                 .format(filename_out, gulp_size, max_dd, min_dd, threshold, n_boxcar, kernel, gpu_id))
+    logger.debug(f"find_et: At entry, filename_out={filename_out}, config={pipeline_config}, gpu_id={gpu_id}")
+
     if h5py.is_hdf5(filename):
         ds = from_h5(filename)
     elif sigproc.is_filterbank(filename):
         ds = from_fil(filename)
+    elif isinstance(stg.Frame, filename):
+        ds = filename 
     else:
         raise RuntimeError("Only HDF5 and filterbank files currently supported")
 
@@ -137,10 +146,7 @@ def find_et(filename, filename_out='hits.csv', gulp_size=2**19, max_dd=4.0, min_
 
     for d_arr in ds.iterate_through_data({'frequency': gulp_size}):
         #print(d_arr)
-        hits = run_pipeline(d_arr, max_dd=max_dd, min_dd=min_dd, 
-                                threshold=threshold, n_boxcar=n_boxcar, 
-                                min_fdistance=min_fdistance, kernel=kernel, 
-                                gpu_id=gpu_id, *args, **kwargs)
+        hits = run_pipeline(d_arr, pipeline_config, gpu_id=gpu_id)
         out.append(hits)
         logger.info(f"{len(hits)} hits found")
     
