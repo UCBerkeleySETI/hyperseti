42b68900cdf41da45f7b06fadaeac4d31e057e09
Danny Price <dan@thetelegraphic.com>
2022-04-29 16:09:35 +0800

    Bumping version to 0.0.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 with open("hyperseti/version.py", "w") as fh:
     fh.write("HYPERSETI_VERSION = '{}'\n".format(__version__))
