737cb7439a3f6d3da18a4db4594929b1ddb19f1d
Danny Price <dan@thetelegraphic.com>
2023-03-19 22:56:56 +0800

    Bugfix: indexing in blank_edges

--- hyperseti/blanking.py
+++ hyperseti/blanking.py
@@ -12,7 +12,7 @@ logger = get_logger('hyperseti.blanking')
 def blank_edges(data_array, n_chan):
     """ Blank n_chan at edge of data array """
     data_array.data[..., :n_chan] = 0
-    data_array.data[..., data_array.data.shape[0] - n_chan:] = 0
+    data_array.data[..., data_array.data.shape[-1] - n_chan:] = 0
     return data_array
 
 def blank_extrema(data_array, threshold, do_neg=False):
